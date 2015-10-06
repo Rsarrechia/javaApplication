5a8941005241e8f2754dfc66b6630d1c21c9674e
rafael <rafael.sarrechia@hotmail.com>
2015-10-06 19:18:16 +0200

    initial commit

---
+++ src/be/pxl/javaApp/main.java
@@ -0,0 +1,17 @@
+package be.pxl.javaApp;
+
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		ArrayList <Integer> lijst = new ArrayList<>();
+
+		while(true) {
+			int teller = 0;
+			lijst.add(teller);
+			teller++;
+		}
+	}
+}
