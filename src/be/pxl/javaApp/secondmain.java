8df035975be191a0135d186fe979096d5b6bf592
rafael <rafael.sarrechia@hotmail.com>
2015-10-06 20:09:11 +0200

    second main gemaakt

---
+++ src/be/pxl/javaApp/secondmain.java
@@ -0,0 +1,20 @@
+package be.pxl.javaApp;
+
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args){
+		// vijfde aanpassing
+		// TODO Auto-generated method stub
+		// derde aanpassing
+		// vierde aanpassing
+		ArrayList <Integer> lijst = new ArrayList<>();
+		// eerste aanpassing
+		while(true) {
+			int teller = 0;
+			lijst.add(teller);
+			teller++;
+		}
+	}
+}
